2101684f2dce563f5bd99454ae654ab34ea2f1a5
opilane <opilane@HP-CZC2349HTQ>
2024-04-10 10:55:20 +0300

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,18 @@
+﻿namespace FileApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				
+                fonts.AddFont("LTPerfume-2.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build();
+	}
+}
